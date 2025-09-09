fb84bbbc62ca5fd946ad955e12b555cfad8d7056
H. Vetinari <h.vetinari@gmx.com>
2025-09-09 15:38:26 +0200

    remove flag that's superfluous with `-Dbuildtype=release` in MESON_ARGS

--- recipe/build_atk.sh
+++ recipe/build_atk.sh
@@ -58,7 +58,6 @@ if [ "${CONDA_BUILD_CROSS_COMPILATION}" = "1" ]; then
 
         meson setup native-build \
             "${meson_config_args[@]}" \
-            --buildtype=release \
             --prefix=$BUILD_PREFIX \
             -Dlibdir=lib \
             --wrap-mode=nofallback
@@ -76,7 +75,6 @@ fi
 meson setup builddir \
     ${MESON_ARGS} \
     "${meson_config_args[@]}" \
-    --buildtype=release \
     --prefix=$PREFIX \
     -Dlibdir=lib \
     --wrap-mode=nofallback
