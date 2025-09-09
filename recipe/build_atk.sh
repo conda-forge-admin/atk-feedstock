73ddfc55adfd85fe27f03db1b46a0ae1d541b9a5
H. Vetinari <h.vetinari@gmx.com>
2025-09-09 15:19:03 +0200

    sync updates from gdk-pixbuf-feedstock
    see https://github.com/conda-forge/gdk-pixbuf-feedstock/blame/main/recipe/build.sh

--- recipe/build_atk.sh
+++ recipe/build_atk.sh
@@ -2,33 +2,25 @@
 
 set -ex
 
-# Follow the pattern of https://github.com/conda-forge/gdk-pixbuf-feedstock/pull/28/
-if [ "${CONDA_BUILD_CROSS_COMPILATION}" = "1" ]; then
-    unset _CONDA_PYTHON_SYSCONFIGDATA_NAME
-    (
-        mkdir -p native-build
-        pushd native-build
+meson_config_args=(
+    --backend=ninja
+    -Ddocs=false
+    -Dgio_sniffing=false
+    -Dinstalled_tests=false
+    -Drelocatable=true
+    -Dintrospection=enabled
+    -Dgtk_doc=false
+    -Dman=false
+    -Dothers=enabled
+)
 
-        export CC=$CC_FOR_BUILD
-        export AR=($CC_FOR_BUILD -print-prog-name=ar)
-        export NM=($CC_FOR_BUILD -print-prog-name=nm)
-        export LDFLAGS=${LDFLAGS//$PREFIX/$BUILD_PREFIX}
-        export PKG_CONFIG_PATH=${BUILD_PREFIX}/lib/pkgconfig
-
-        # Unset them as we're ok with builds that are either slow or non-portable
-        unset CFLAGS
-        unset CPPFLAGS
-
-        meson --libdir=$BUILD_PREFIX/lib --prefix=$BUILD_PREFIX ..
-        # This script would generate the functions.txt and dump.xml and save them
-        # This is loaded in the native build. We assume that the functions exported
-        # by glib are the same for the native and cross builds
-        export GI_CROSS_LAUNCHER=$BUILD_PREFIX/libexec/gi-cross-launcher-save.sh
-        ninja -j$CPU_COUNT -v
-        ninja install
-        popd
-    )
-    export GI_CROSS_LAUNCHER=$BUILD_PREFIX/libexec/gi-cross-launcher-load.sh
+if [[ "$target_platform" = osx-* ]] ; then
+    # The -dead_strip_dylibs option breaks g-ir-scanner in this package: the
+    # scanner links a test executable to find paths to dylibs, but with this
+    # option the linker strips them out. The resulting error message is
+    # "ERROR: can't resolve libraries to shared libraries: ...".
+    export LDFLAGS="$(echo $LDFLAGS |sed -e "s/-Wl,-dead_strip_dylibs//g")"
+    export LDFLAGS_LD="$(echo $LDFLAGS_LD |sed -e "s/-dead_strip_dylibs//g")"
 fi
 
 # get meson to find pkg-config when cross compiling
@@ -41,10 +33,55 @@ export PKG_CONFIG=$BUILD_PREFIX/bin/pkg-config
 export PKG_CONFIG_PATH_FOR_BUILD=$BUILD_PREFIX/lib/pkgconfig
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$BUILD_PREFIX/lib/pkgconfig
 
-meson ${MESON_ARGS:---libdir=$PREFIX/lib} builddir --prefix=$PREFIX
-meson configure -Ddocs=false builddir
-ninja -v -C builddir
-ninja -C builddir install
+export XDG_DATA_DIRS=${XDG_DATA_DIRS}:$PREFIX/share
+
+# ensure that the post install script is ignored
+export DESTDIR="/"
+
+# Follow the pattern of https://github.com/conda-forge/gdk-pixbuf-feedstock/pull/28/
+if [ "${CONDA_BUILD_CROSS_COMPILATION}" = "1" ]; then
+    unset _CONDA_PYTHON_SYSCONFIGDATA_NAME
+    (
+        mkdir -p native-build
+
+        export CC=$CC_FOR_BUILD
+        export AR="$($CC_FOR_BUILD -print-prog-name=ar)"
+        export NM="$($CC_FOR_BUILD -print-prog-name=nm)"
+        export LDFLAGS=${LDFLAGS//$PREFIX/$BUILD_PREFIX}
+        export PKG_CONFIG_PATH=${BUILD_PREFIX}/lib/pkgconfig
+
+        # Unset them as we're ok with builds that are either slow or non-portable
+        unset CFLAGS
+        unset CPPFLAGS
+        export host_alias=$build_alias
+        export PKG_CONFIG_PATH=$BUILD_PREFIX/lib/pkgconfig
+
+        meson setup native-build \
+            "${meson_config_args[@]}" \
+            --buildtype=release \
+            --prefix=$BUILD_PREFIX \
+            -Dlibdir=lib \
+            --wrap-mode=nofallback
+
+        # This script would generate the functions.txt and dump.xml and save them
+        # This is loaded in the native build. We assume that the functions exported
+        # by glib are the same for the native and cross builds
+        export GI_CROSS_LAUNCHER=$BUILD_PREFIX/libexec/gi-cross-launcher-save.sh
+        ninja -j$CPU_COUNT -v
+        ninja install
+    )
+    export GI_CROSS_LAUNCHER=$BUILD_PREFIX/libexec/gi-cross-launcher-load.sh
+fi
+
+meson setup builddir \
+    ${MESON_ARGS} \
+    "${meson_config_args[@]}" \
+    --buildtype=release \
+    --prefix=$PREFIX \
+    -Dlibdir=lib \
+    --wrap-mode=nofallback
+ninja -v -C builddir -j ${CPU_COUNT}
+ninja -C builddir install -j ${CPU_COUNT}
 
 cd $PREFIX
 find . -type f -name "*.la" -exec rm -rf '{}' \; -print
